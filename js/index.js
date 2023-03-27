7f77908187270453c97613d81a3f3b71fe0448e1
BlackJackMC <duytu85343@gmail.com>
2023-03-28 05:59:12 +0700

    Update something idk

--- js/index.js
+++ js/index.js
@@ -1,11 +1,12 @@
 
 $(window).on("load", function () {
-    let element = $("#main");
+    let element = $("header");
     let original = element.offset().top;
 
-    $(window).scroll(function () {
+    $(window).on("scroll", function () {
         let img = $("#logo");
         let scrollTop = $(window).scrollTop();
+        // console.log(original, scrollTop);
         if (scrollTop > original) {
             img.css("inset", "auto 0 auto 0");
         } else {
