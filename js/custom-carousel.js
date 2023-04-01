8e4a6e4e6e433375b2d30c5a1b3dbdfc19c760c2
BlackJackMC <duytu85343@gmail.com>
2023-04-01 14:57:27 +0700

    Waste 3hrs to make a carousel

---
+++ js/custom-carousel.js
@@ -0,0 +1,6 @@
+$("#category .owl-carousel.slider").owlCarousel({
+    nav: true,
+    rewind: true,
+    margin: 0,
+    navText: ["<div class=\"circle\"><i class=\"fa-solid fa-angle-left\"></i></div>", "<div class=\"circle\"><i class=\"fa-solid fa-angle-right\"></i></div>"]
+})
